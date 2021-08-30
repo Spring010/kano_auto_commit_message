c7444bdbafbc87209d65a823d0c8653f524056ff
Yitong <yitongli2014@gmail.com>
2021-08-30 12:27:35 -0700

    clean data v1-dropped

--- repo.py
+++ repo.py
@@ -11,14 +11,34 @@ repo = Repo.init('/Users/yitongli/pytorch')
 
 #Reference:https://azzamsa.com/n/gitpython-intro/
 #Unwrapped git functionality
-logs=repo.git.log("-p","--raw")
+logs=repo.git.log("-p","--format=' }\n{'Hash': '%H', 'Commit-m': '%f', 'Summary': '%b',\n 'gitdiff': '")
 logs_split = logs.splitlines()
 type(logs_split)
-for i in logs_split[500:600]:
+# with open("txt.txt", "w") as f:
+#     for t in logs_split[:1000]:
+#         print(t, file=f)
+
+
+# ID_mark = [ i for i in range(len(logs_split)) if logs_split[i] == '<<<<<<<ID>>>>>>>' ]
+# gitdiff_mark = [ i[0:11] for i in logs_split]
+# gitdiff_mark_index = [gitdiff_mark.index('diff --git ',ID_mark[i]) for i in range(len(ID_mark))]
+# logs_split_cleaner = logs_split
+# for i in gitdiff_mark_index:
+#     logs_split_cleaner[i-1] = 'gitdiff:'
+#
+#
+# filter(lambda x: ' A  ' in x, logs_split_cleaner)
+logs_split_cleaner = logs_split
+for i in logs_split_cleaner[:1000]:
     print(i)
 
-logs_H=repo.git.log("--pretty=format:(%H,%f,%b)")
-logs_split_H = logs_H.splitlines()
-for i in logs_split_H[:50]:
-    print(i)
+
+
+
+# logs_H=repo.git.log("--pretty=format:(%H,%f,%b)")
+# logs_split_H = logs_H.splitlines()
+# for i in logs_split_H[:50]:
+#     print(i)
+
+
 
